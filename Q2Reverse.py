636c16e0a20bf5e6c770cd0d413f43f274e4f171
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-05-05 10:56:46 -0400

    Update Q2Reverse.py

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,11 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+
+while True: 
+    word = input("Enter a word or type quit to exit: ")
+    if word == "quit":
+        break
+    if word.isalpha() and " " not in word: 
+      for i in range(len(word)-1, -1, -1): 
+        print(word[i]) 
+    else: 
+      print("Invalid input. Please enter a single word with only letters.") 

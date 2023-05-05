afb7a18f8a171c24d59d54c0739037a975262591
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-05-05 10:51:19 -0400

    Update Q1Length.py

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,9 @@
-# Create a program that will accept a word from a user and return the length of that word. 
-# Make this program in a loop with option to exit when the use types in quit.
+while True:
+  word = input("Enter a word or type quit to exit: ")
+  if word == "quit":
+    break
+  elif word.isalpha() and " " not in word:
+    length = len(word)
+    print(f"There are {length} characters in the word \"{word}\"")
+  else:
+    print("Invalid input. Please enter a single word with only letters.")
